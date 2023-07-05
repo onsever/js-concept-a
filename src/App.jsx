b2f5ad8d3c98b2f0714103abe66cb08124466d53
Onurcan Sever <onsever@icloud.com>
2023-07-05 13:32:08 -0400

    create functions and students array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import "./App.css";
 
 export default function App() {
-    return <h1>Hello world!</h1>
+    const students = [
+        { name: "John", age: 20, grades: [50, 75, 89] },
+        { name: "Gerald", age: 25, grades: [88, 34, 93] },
+        { name: "Khan", age: 22, grades: [44, 21, 37] },
+    ];
+
+    const calculateAverageGrade = (grades) => {
+        const sum = grades.reduce((total, grade) => total + grade, 0);
+        return sum / grades.length;
+    }
+
+    const printStudentInfo = (student) => {
+        const averageGrade = calculateAverageGrade(student.grades);
+        console.log(`${student.name} (${student.age}) - ${averageGrade}`);
+    }
+
+    students.forEach(printStudentInfo);
+
+    return (
+        <div>
+            Hello World
+        </div>
+    );
 }
